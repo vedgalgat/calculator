d94eaec3fd8f7b18e190e1b3d4ea9e7df071c8cc
vedgalgat <164989204+vedgalgat@users.noreply.github.com>
2024-03-29 21:07:38 +0530

    this is my calcutator application

---
+++ app.js
@@ -0,0 +1,29 @@
+  let input = document.querySelector("#inputvalue")
+  let buttons = document.querySelectorAll("button")
+
+  let string = "";
+
+  Array.from(buttons).forEach(button => {
+    button.addEventListener("click", (e) => {
+      if (e.target.innerHTML === "=") {
+        string = eval(string)
+        input.value = string
+        string=""
+      }
+      else if(e.target.innerHTML==="AC"){
+        string=""
+        input.value=string
+      }
+      else if(e.target.innerHTML=="DEL"){
+        string=string.substring(0,string.length-1)
+        input.value=string
+      }
+      
+      else {
+        string += e.target.innerHTML
+        input.value = string
+
+      }
+
+    })
+  })
